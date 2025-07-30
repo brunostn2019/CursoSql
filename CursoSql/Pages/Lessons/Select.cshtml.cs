cfbe6e9fbc759891ad7489574cc62cadbcc12f31
brunostn2019 <bruno.lico@gmail.com>
2025-07-29 22:33:02 -0300

    Commit inicial

---
+++ CursoSql/Pages/Lessons/Select.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CursoSql.Pages.Lessons
+{
+    public class SelectModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
